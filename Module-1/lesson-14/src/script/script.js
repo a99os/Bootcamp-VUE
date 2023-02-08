c2613aa3e6c334b878166810a7c225eefd507e74
a99os <abbosashirboev@gmail.com>
2023-02-08 13:40:20 +0500

    add bookshelter project

--- Module-1/lesson-14/src/script/script.js
+++ Module-1/lesson-14/src/script/script.js
@@ -3,7 +3,7 @@
 let them = document.querySelector("#them");
 let header = document.querySelector("header");
 let search = document.querySelector("#search");
-let region = document.querySelector("#region");
+let regionSelect = document.querySelector("#region");
 let cardTemplate = document.querySelector(".card");
 let cardWrapper = document.querySelector(".card__wrapper");
 them.addEventListener("input", (e) => {
@@ -32,6 +32,8 @@ changemode();
 
 // ! ------------dinamic cards --------------------
 let baseURL = "https://restcountries.com/v2/all";
+let filterURL = "https://restcountries.com/v2/region";
+let searchURL = "https://restcountries.com/v2/name";
 
 const getAllCountries = async () => {
   cardWrapper.innerHTML = `<span class="loader"></span>`;
@@ -40,6 +42,7 @@ const getAllCountries = async () => {
     const result = await response.json();
     if (response.status == 200) {
       renderCards(result);
+      filterRegion(result);
     } else {
       alert(response.status);
     }
@@ -47,7 +50,8 @@ const getAllCountries = async () => {
     console.log(error);
   }
 };
-getAllCountries();
+
+getAllCountries(baseURL);
 
 // ! ------------render cards --------------------
 
@@ -66,14 +70,93 @@ function renderCards(cards) {
           />
         </a>
         <div class="p-6">
-          <h5 class="text-xl font-medium mb-2">${element.name}</h5>
+          <h5 class="text-xl font-medium mb-2 card__title cursor-pointer" data-isname =${
+            element.name
+          }>${element.name}</h5>
           <li class="list-none">
             <li><strong>Population:</strong>${element.population}</li>
             <li><strong>Region:</strong>${element.region}</li>
-            <li><strong>Capital:</strong>${element.capital}</li>
+            <li><strong>Capital:</strong>${
+              element.capital || "not found capital"
+            }</li>
           </ul>
         </div>`
     );
+    card.dataset.isname = element.name;
     cardWrapper.append(card);
   });
 }
+
+// !------------------dinamic selection  region------------------------
+function filterRegion(data) {
+  const region = [];
+  data.forEach((item) => {
+    if (!region.includes(item.region)) {
+      region.push(item.region);
+    }
+  });
+  region.sort();
+  region.forEach((item) => {
+    const element = createElement("option", "item", item);
+    element.value = item;
+    regionSelect.append(element);
+  });
+  console.log(region);
+}
+
+const getFillterCountries = async (region) => {
+  cardWrapper.innerHTML = `<span class="loader"></span>`;
+  try {
+    const response = await fetch(filterURL + `/${region}`);
+    const result = await response.json();
+    if (response.status == 200) {
+      renderCards(result);
+    } else {
+      alert(response.status);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+regionSelect.addEventListener("change", (e) => {
+  getFillterCountries(e.target.value);
+  console.log("salom");
+});
+
+// ! ----------------search ------------------
+async function searchCountries(country) {
+  cardWrapper.innerHTML = `<span class="loader"></span>`;
+  try {
+    const response = await fetch(searchURL + `/${country}`);
+    const result = await response.json();
+    if (response.status == 200) {
+      renderCards(result);
+    } else {
+      cardWrapper.innerHTML = "";
+      cardWrapper.innerHTML =
+        "<h1 class=' uppercase text-lg text-red-500 tetx-center'> Not found region</h1>";
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+let searchInput = document.querySelector("#search");
+search.addEventListener("keyup", (e) => {
+  if (!e.target.value.trim().length) {
+    getAllCountries();
+  } else {
+    searchCountries(e.target.value.trim());
+    search.setAttribute("placeholder", "Pleace fill region name");
+  }
+});
+
+// !------
+cardWrapper.addEventListener("click", (e) => {
+  if (e.target.classList.contains("card__title")) {
+    const isname = e.target.getAttribute("data-isname");
+    console.log(isname);
+    localStorage.setItem("isname", isname);
+    window.open("./country.html");
+  }
+});
