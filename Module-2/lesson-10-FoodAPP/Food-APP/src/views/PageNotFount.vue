6ff842d72124f90012b22c79e4ff1391e7b6e708
a99os <abbosashirboev@gmail.com>
2023-02-28 22:39:01 +0500

    add error correction

--- Module-2/lesson-10-FoodAPP/Food-APP/src/views/PageNotFount.vue
+++ Module-2/lesson-10-FoodAPP/Food-APP/src/views/PageNotFount.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div class="w-full h-screen flex items-center justify-center p-6 overflow-hidden bg-[#252836] text-white">
-    <h1 class="text-center text-3xl">Page Not Fount-404</h1>
+    <h1 class="text-center text-3xl">Page Not Found-404</h1>
   </div>
 </template>
 <script>
