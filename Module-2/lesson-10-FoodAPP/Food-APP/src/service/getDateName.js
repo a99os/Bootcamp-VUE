b56322ba6d3771db7c5e9e4c283310dcdb02cc7f
a99os <abbosashirboev@gmail.com>
2023-03-07 23:34:40 +0500

    add role beckend

--- Module-2/lesson-10-FoodAPP/Food-APP/src/service/getDateName.js
+++ Module-2/lesson-10-FoodAPP/Food-APP/src/service/getDateName.js
@@ -9,5 +9,5 @@ var days = [
 ];
 var d = new Date();
 export const weekName = days[d.getDay()];
-const date = new Date(); // 2009-11-10
+const date = new Date();
 export const monthName = date.toLocaleString("default", { month: "short" });
